50da8eb4db40487f502cf512df17c1a2288307ea
philippe lang <lang.philippe@gmail.con>
2012-08-21 12:53:27 +0200

    fixe tpl2 bugs

--- nodejs/tpl2.js
+++ nodejs/tpl2.js
@@ -3,6 +3,7 @@ var net = require('net');
 var ntm;
 
 var CmdCompletePattern= new RegExp("10 COMMAND COMPLETE","m");
+var CmdSetCompletePattern= new RegExp("10 COMMAND COMPLETE","m");
 
 var HAPattern = new RegExp			("10 DATA INLINE HA.CURRPOS=(-?[0-9]*.[0-9]*)","m");
 var DECPattern = new RegExp			("10 DATA INLINE DEC.CURRPOS=(-?[0-9]*.[0-9]*)","m");
@@ -30,12 +31,12 @@ var TelescopStatus = {
 	
 exports.powerOn = function(){
 	ntmAnwser = "";
-	ntm.write("100 SET CABINET.POWER_STATE=1\n");
+	ntm.write("100 SET CABINET.POWER=1\n");
 }
 
 exports.powerOff = function(){
 	ntmAnwser = "";
-	ntm.write("100 SET CABINET.POWER_STATE=0\n");
+	ntm.write("100 SET CABINET.POWER=0\n");
 }
 
 exports.park = function(){
@@ -44,26 +45,28 @@ exports.park = function(){
 }
 
 
-export.slew(ra,dec) {
+exports.slew = function(ra,dec) {
 	ntmAnwser = "";
-	ntm.write("100 POINTING.TARGET.RA"+ra+"\n");
-	ntm.write("100 POINTING.TARGET.DEC"+dec+"\n");
-	ntm.write("100 POINTING.TARGET.RA_V=0\n");
-	ntm.write("100 POINTING.TARGET.DEC_V=0\n");
-	ntm.write("100 POINTING.TRACK=386\n");
+	
+	ntm.write("100 SET POINTING.TARGET.RA="+ra.toString()+"\n");
+	ntm.write("100 SET POINTING.TARGET.DEC="+dec.toString()+"\n");
+	ntm.write("100 SET POINTING.TARGET.RA_V=0.0\n");
+	ntm.write("100 SET POINTING.TARGET.DEC_V=0.0\n");
+	ntm.write("100 SET POINTING.TRACK=386\n");
+	
 }
 
-export.StartTrack(){
+exports.startTrack= function(){
 	ntmAnwser = "";
-	ntm.write("100 POINTING.TRACK=386\n");
+	ntm.write("100 SET POINTING.TRACK=386\n");
 }
 
-export.StopTrack()
+exports.stopTrack= function()
 {
 	ntmAnwser = "";
-	ntm.write("100 POINTING.TRACK=0\n");
+	ntm.write("100 SET POINTING.TRACK=0\n");
 }
-export.clearError() {
+exports.clearError = function(){
 	ntmAnwser = "";
 	ntm.write("100 SET CABINET.STATUS.CLER=1\n");
 
@@ -91,11 +94,11 @@ exports.NTMConnect = function () {
 	 TelescopStatus.CurrHA   = parseFloat(ntmAnwser.match(HAPattern)[1]);
 	 TelescopStatus.CurrDec  = parseFloat(ntmAnwser.match(DECPattern)[1]);
 	 TelescopStatus.Power= parseFloat(ntmAnwser.match(PowerStatePattern)[1]);
-	 TelescopStatus.Referenced= parseFloat(ntmAnwser.match(ReferendedPattern)[1]);
+	 TelescopStatus.Referenced= parseInt(ntmAnwser.match(ReferendedPattern)[1]);
 	 TelescopStatus.Globalstatus= parseFloat(ntmAnwser.match(GlobalStatusPattern)[1]);
-	 TelescopStatus.Track= parseFloat(ntmAnwser.match(TrackPattern)[1]);
-	 TelescopStatus.HAMotionstate= parseFloat(ntmAnwser.match(HAMotionPattern)[1]);
-	 TelescopStatus.DECMotionstate= parseFloat(ntmAnwser.match(DECMotionPattern)[1]);
+	 TelescopStatus.Track= parseInt(ntmAnwser.match(TrackPattern)[1]);
+	 TelescopStatus.HAMotionstate= parseInt(ntmAnwser.match(HAMotionPattern)[1]);
+	 TelescopStatus.DECMotionstate= parseInt(ntmAnwser.match(DECMotionPattern)[1]);
 	 console.log(TelescopStatus);
 	 ntmAnwser = "";
 	 }
@@ -115,8 +118,5 @@ exports.NTMDisconnect = function() {
 }
 
 
-exports.NTMConnect();
-var statusId=setInterval(exports.getNTMStatus,2000);
-setTimeout(exports.NTMDisconnect,20000);
 
 
